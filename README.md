29f84e4ad2c74dc4075513fb63105410713a3fb4
shivneutral <20592shivkumar@gmail.com>
2021-10-10 14:31:42 +0530

    add library

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # phpckeditor
 this is totally based on ck editor library
+
+
+download always official web site
+https://ckeditor.com/ckfinder/
+
+point 
+1.first you can download ckeditor file from 
+https://ckeditor.com/ckeditor-4/download/?null-addons=
+2. config localsystem 
+3. if you use php than you can use header and session to better response
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+if(isset($_SESSION['msg'])){
+ echo $_SESSION['msg'];
+ unset($_SESSION['msg']);
+}
+
+if(isset($_POST['submit'])){
+    $conn=mysqli_connect('127.0.0.1:3307','root','','ckeditor');
+   
+    $adt=date('Y-m-d h:i:s');
+    $n="shiv";
+    $cnt=$_POST['ckeditortxt'];
+    $sql="INSERT INTO `editortab`(`name`, `content`, `createdat`) VALUES ('$n','$cnt','$adt')";
+    
+    if(mysqli_query($conn,$sql)){
+        $_SESSION['msg']='data inserted';
+        header('location:index.php');
+        die();
+         echo "successfully"; 
+    }else{
+        echo "please try again";
+    }
+    
+}
+
+
+?>
+
+
+<html>
+    <head>
+        <title>ckeditor</title>
+        <script src="ckeditor/ckeditor.js"></script>
+    </head>
+    <body>
+               <form action="#" method="post">
+                   <textarea id="ckeditortxt" name="ckeditortxt">
+
+                   </textarea>
+                   <input type="submit" name="submit">
+
+               </form>
+               <script>
+                   CKEDITOR.replace('ckeditortxt');
+                 //  config.disableNativeSpellChecker=true;
+                  // config.scayt_autoStartup = true;
+
+               </script>
+               
+    </body>
+</html>
